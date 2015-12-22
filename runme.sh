fd5d347567a4eef9ecd0562fb3eb601d1f5fadd4
Jordi Prats <jprats@systemadmin.es>
2015-12-22 16:56:47 +0100

    canvi identació per si de cas

--- runme.sh
+++ runme.sh
@@ -5,10 +5,9 @@ mkdir -p /modules/logs/
 exec > /modules/logs/activemq.startup.${EYP_INSTANCE_NUMBER}.log 2>&1
 
 cat <<EOF > /tmp/manifest.pp
-
 class { 'mcollective::activemq':
-	adminpw => 'admin',
-	userpw => '${EYP_ACTIVEMQ_ADMIN_PASSWORD-Y2F0YWx1bnlhbGxpdXJlCg}',
+adminpw => 'admin',
+userpw => '${EYP_ACTIVEMQ_ADMIN_PASSWORD-Y2F0YWx1bnlhbGxpdXJlCg}',
 }
 EOF
 
