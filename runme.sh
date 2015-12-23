cdc8a40a1702b49553da278b503f9fad8ddba8ec
Jordi Prats <jprats@systemadmin.es>
2015-12-23 10:02:16 +0100

    identació

--- runme.sh
+++ runme.sh
@@ -6,8 +6,8 @@ exec > /logs/logs/activemq.startup.${EYP_INSTANCE_NUMBER}.log 2>&1
 
 cat <<EOF > /tmp/manifest.pp
 class { 'mcollective::activemq':
-adminpw => 'admin',
-userpw => '${EYP_ACTIVEMQ_ADMIN_PASSWORD-Y2F0YWx1bnlhbGxpdXJlCg}',
+ adminpw => 'admin',
+ userpw => '${EYP_ACTIVEMQ_ADMIN_PASSWORD-Y2F0YWx1bnlhbGxpdXJlCg}',
 }
 EOF
 
